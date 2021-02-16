fb506e52525b8a8f5fea9f5dc4dbefa42e31018f
itssanat <yoursamit15@gmail.com>
2021-02-16 13:07:54 +0530

    model added

--- webapp/dfd/views.py
+++ webapp/dfd/views.py
@@ -6,8 +6,54 @@ import shutil
 import time
 import os
 
+# ---------------------------------
+import tensorflow as tf
+import cv2
+import os
+import numpy as np
+from PIL import Image, ImageChops, ImageEnhance
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import img_to_array, load_img
+import dlib
+
+# --------------------------------
+model = load_model('/home/itssanat/Downloads/deepfake-detection-model.h5')
+input_shape = (128, 128, 3)
+detector = dlib.get_frontal_face_detector()
+
+def predict(request):
+    print('inside predict function')
+    print(request.session['file_name'])
+    cap = cv2.VideoCapture(request.session['file_name'])
+    frameRate = cap.get(5)
+    while cap.isOpened():
+        frameId = cap.get(1)
+        ret, frame = cap.read()
+        if ret != True:
+            break
+        if frameId % ((int(frameRate)+1)*1) == 0:
+            face_rects, scores, idx = detector.run(frame, 0)
+            for i, d in enumerate(face_rects):
+                x1 = d.left()
+                y1 = d.top()
+                x2 = d.right()
+                y2 = d.bottom()
+                crop_img = frame[y1:y2, x1:x2]
+                data = img_to_array(cv2.resize(crop_img, (128, 128))).flatten() / 255.0
+                data = data.reshape(-1, 128, 128, 3)
+                print(model.predict_classes(data))
+
+
 def index(request):
-    if request.method == 'POST':
+    if request.method == 'GET':
+        form = VideoForm()
+        if 'file_name' in request.session:
+            del request.session['file_name']
+        if 'faces_cropped_images' in request.session:
+            del request.session['faces_cropped_images']
+        return render(request , 'dfd/index.html' , {'form': form})
+        
+    else :
         form = VideoForm(request.POST, request.FILES)
         # print(form.is_valid())
         if form.is_valid():
@@ -24,7 +70,7 @@ def index(request):
                 shutil.copyfileobj(video_file, vFile)
             request.session['file_name'] = os.path.join(settings.BASE_DIR, 'temp_videos', saved_video_file)
             print(request.session['file_name'])
-        
-    else:
-        form = VideoForm()
-    return render(request , 'dfd/index.html' , {'form': form})
+            predict(request)
+
+        return render(request , 'dfd/index.html' , {'form': form})
+             
